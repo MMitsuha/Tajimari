9b70ec8c6ce97b866e923dd5cb48f496a2380b85
宮水_五葉 <caizhe666@qq.com>
2023-03-25 14:19:22 +0800

    update

--- Tajimari/main.cpp
+++ Tajimari/main.cpp
@@ -74,43 +74,8 @@ wmain(
 		CloseHandle(pi.hProcess);
 		CloseHandle(pi.hThread);
 	}
+	else
+		spdlog::error("Error starting Modified.exe, error code: 0x{:x}", GetLastError());
 
 	return 0;
 }
-
-/*
-auto imports = objTarget.enumImport();
-objTarget.setImport(imports);
-
-// Get and copy the section
-auto secText = objTemplate.getSectionByName(".text");
-auto exports = objTemplate.enumExport();
-uint32_t offsetMain = 0;
-uint32_t offsetVar = 0;
-for (auto& exp : exports) {
-	if (exp.Name == "main") {
-		offsetMain = exp.Rva - secText.VirtualAddress;
-		spdlog::info("Found main at offset: 0x{:x}", offsetMain);
-	}
-	if (exp.Name == "OriginalEntryPoint") {
-		offsetVar = exp.Rva - secText.VirtualAddress;
-		spdlog::info("Found var at offset: 0x{:x}", offsetVar);
-	}
-}
-
-auto& secAdded = objTarget.getSectionHeaders().emplace_back(secText);
-secAdded.Name[0] = '.';
-secAdded.Name[1] = 'h';
-secAdded.Name[2] = 'a';
-secAdded.Name[3] = 'c';
-secAdded.Name[4] = 'k';
-objTarget.updateHeaders();
-
-auto vaOriginalMain = objTarget.getNtHeaders().getOptionalHeader().ImageBase + objTarget.getNtHeaders().getOptionalHeader().AddressOfEntryPoint;
-auto rvaNewMain = secAdded.VirtualAddress + offsetMain;
-objTarget.getNtHeaders().getOptionalHeader().AddressOfEntryPoint = rvaNewMain;
-(*(uintptr_t*)(secAdded.m_content.data() + offsetVar)) = vaOriginalMain;
-
-objTarget.rebuild();
-objTarget.write(R"(.\Modified.exe)");
-*/
